2fe210e2609baa156ef74ec59a60cd96b9d5e8e5
Jimmy Connors <jjc9bb@virginia.edu>
2021-10-15 20:21:15 -0400

    Initial Commit
    This is what we did on day 1!

---
+++ Assets/movement.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float moveSpeed = 5f;
+    public float turnSpeed = 5f;
+    public Rigidbody2D rb;
+
+    Vector2 movementVector;
+    
+    
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        movementVector.x = Input.GetAxisRaw("Horizontal");
+        movementVector.y = Input.GetAxisRaw("Vertical");
+        
+    }
+
+    private void FixedUpdate()
+    {
+        if (movementVector.y > 0)
+        {
+            rb.AddForce(transform.up * moveSpeed * movementVector.y);
+        }else if(movementVector.y < 0)
+        {
+            rb.AddForce(transform.up * 0.5f * moveSpeed * movementVector.y);
+        }
+
+        if(movementVector.x != 0)
+        {
+            rb.rotation -= movementVector.x*turnSpeed;
+        }
+
+       // rb.MovePosition(rb.position + movementVector * moveSpeed * Time.fixedDeltaTime);
+    }
+}
---
+++ Assets/sonar.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class sonar : MonoBehaviour
+{
+    float rot = 0f;
+    public float rotationPerUpdate = 0.001f;
+    public Transform boat;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(0, 0, rotationPerUpdate);
+        transform.position = boat.position;
+    }
+
+    private void FixedUpdate()
+    {
+        rot += rotationPerUpdate;
+    }
+}
