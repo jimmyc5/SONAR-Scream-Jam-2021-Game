f50fbf9f691513d1400c9e40c82bb625520d7f62
Chris Ricigliano <cmr4jw@virginia.edu>
2021-10-17 04:57:44 -0400

    update
    -moved boat to layer 4
    -gave boat and sonar line a collison box
    -orgainzed asset file strucutre
    -made blip prefabs for two colors, yellow and red, with adjustable visable timers
    -made a temporary game over when colliding with red blip

---
+++ Assets/script/dotSpawner.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class dotSpawner : MonoBehaviour
+{
+ //how long blip is displayed after being sonared
+    public float displayTime = 1.0f;
+
+    //to detect collsions
+    public Rigidbody2D rb;
+
+    //true if detected
+    public bool detected = false;
+
+    //where blip is spawned
+    Transform spawnPoint;
+
+    //gameobject to spawn when detected (what color blip to spawn)
+    public GameObject gb;
+
+    void Start()
+    {
+     //0f sets opacity to 0 when sprite is first drawn  to be sure invisable
+      gameObject.GetComponent<SpriteRenderer>().color = new Color(1f,1f,1f,0.0f);
+
+      spawnPoint = gameObject.GetComponent<Transform>();    
+    }
+
+    void Update()
+    {
+        
+    }
+
+    //when hit by sonar beam draws image of self where it was scanned
+    void OnTriggerEnter2D(Collider2D target)
+    {    
+        //check if collided with sonar line
+        if(target.name.Equals("radar"))
+        {
+            //if spawn the blip in place
+            GameObject newObject = Instantiate(gb, spawnPoint.position, spawnPoint.rotation) as GameObject;
+            newObject.GetComponent<blip>().displayTime = displayTime;
+            
+        }
+    }
+}
