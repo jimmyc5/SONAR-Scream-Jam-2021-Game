799ba63d7cedf6cb334674b86c99d099611adeab
Chris Ricigliano <cmr4jw@virginia.edu>
2021-10-17 21:53:00 -0400

    scoring
    -created purple blips (the people to rescue)
    -purple blips begin to move toward boat when you get close enough
    -added simple score and high score (only show in debug menu after game over)
    -when purple blips contact the boat they are removed and added to score

--- Assets/script/gameOver.cs
+++ Assets/script/gameOver.cs
@@ -7,12 +7,12 @@ public class gameOver : MonoBehaviour
   //when hit player ends the game
     void OnTriggerEnter2D(Collider2D target)
     {    
-        Debug.Log("nuts");
         //check if collided with player
         if(target.name.Equals("boat"))
         {
             //PLACE HOLDER
             //pauses the game indefinitly
+            GameObject.Find("Score").GetComponent<scoreTracker>().endGame();
             Time.timeScale =0;            
         }
     }
