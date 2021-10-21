018c2e426cdc7704f472820ef7e4180c502fce20
Spencer <suhSpence53@gmail.com>
2021-10-21 17:58:45 -0400

    camera and boundBox

---
+++ Assets/script/cameraFollow.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraFollow : MonoBehaviour
+{
+    public Transform target;
+    public Vector3 offset;
+    // Start is called before the first frame update
+    void Start()
+    {
+        target = GameObject.Find("boat").GetComponent<Transform>();
+        transform.position = target.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(target){
+            transform.position = target.position + offset;
+        }
+    }        
+}
