549f4d648a5beb54032dc43970f964c4c11d133c
Spencer <suhSpence53@gmail.com>
2021-10-21 19:32:38 -0400

    spanwing! ues!

--- Assets/script/scoreTracker.cs
+++ Assets/script/scoreTracker.cs
@@ -5,19 +5,51 @@ using UnityEngine.SceneManagement;
 //adapted from brackeys "How to make a HIGH SCORE in Unity"
 public class scoreTracker : MonoBehaviour
 {
+    public GameObject player;
     public int score;
     public int highScore;
+    public GameObject[] blips;
+
+
     // Start is called before the first frame update
     void Start()
     {
+        player = GameObject.Find("boat");
         score = 0;
         highScore = PlayerPrefs.GetInt("HighScore", 0);
+        
+        //randomly place things
+        for(int i = 0; i <= 10; i++){
+            SpawnThing(blips[0]);
+        }
+        for(int i = 0; i <= 5; i++){
+            SpawnThing(blips[1]);
+        }
+        for(int i = 0; i <= 7; i++){
+            SpawnThing(blips[2]);
+        }
+
     }
 
     void Update(){
-        if(score == 10){
+        if(score >= 10){
             victory();
         }
+    }
+
+    public void SpawnThing(GameObject type) {
+        GameObject a = Instantiate(type, GenerateSpawnPos(), type.transform.rotation);
+    }
+
+    private Vector3 GenerateSpawnPos() {
+        Collider2D hit;
+        Vector3 location;
+        do{
+            location = player.transform.position + (new Vector3(UnityEngine.Random.Range(-1f, 1f), UnityEngine.Random.Range(-1f,1f), 0f).normalized) * Random.Range(5f, 40f);
+            hit = Physics2D.OverlapCircle(location, 1f);
+        } while(hit); //forces re-roll of location if would spawn zombie in collision
+        
+        return location;
     }
 
     public void endGame()
