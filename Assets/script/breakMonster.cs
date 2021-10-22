0af8bd4bfdbe729becb26c864bedf2b79e1e7856
Chris Ricigliano <cmr4jw@virginia.edu>
2021-10-21 20:53:11 -0400

    rock break monster sound effect and swimmer png update

--- Assets/script/breakMonster.cs
+++ Assets/script/breakMonster.cs
@@ -4,16 +4,19 @@ using UnityEngine;
 
 public class breakMonster : MonoBehaviour
 {
+    public GameObject gb;
     // Start is called before the first frame update
     void Start()
     {
-        
+           
     }
 
  void OnCollisionEnter2D(Collision2D target)
     {    
         if(target.gameObject.GetComponent<gameOver>() != null)
         {
+            Vector3 myVector = new Vector3(0.0f, 1.0f, 0.0f);
+            GameObject newObject = Instantiate(gb, gameObject.GetComponent<Transform>().position, gameObject.GetComponent<Transform>().rotation) as GameObject;
             Destroy(target.gameObject);
             Destroy(gameObject);
         }
